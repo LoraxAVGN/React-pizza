154cfa184e2742f091cee358c929ec91b6c3097a
Kostya <krot.kostya.krot@gmail.comm>
2021-09-24 12:43:43 +0300

    6 lesson react-redux

--- src/App.js
+++ src/App.js
@@ -3,25 +3,42 @@ import {Home, Cart} from "./pages";
 import {Route} from "react-router-dom";
 import React from "react";
 import axios from "axios";
+import {setPizzas as setPizzasAction} from "./redux/actions/pizzas"
+import { connect } from 'react-redux'
 
-function App() {
-    const [pizzaArr, setPizzas] = React.useState([])
+class App extends React.Component{
 
-    React.useEffect(() => {
-        axios.get("http://localhost:3001/db.json").then(({data}) => {setPizzas(data.pizzas)});
-        }, []);
+    componentDidMount() {
+        axios.get("http://localhost:3000/db.json").then(({data}) => {
+            this.props.setPizzas(data.pizzas)
+        });
+    }
 
-  return (
-    <div className="App">
-      <div className="wrapper">
-          <Header />
-          <div className="content">
-              <Route exact path='/' render={() => <Home items={pizzaArr}/>}/>
-              <Route path='/cart' component={Cart}/>
-          </div>
-      </div>
-    </div>
-  );
+    render() {
+        return(
+            <div className="App">
+                <div className="wrapper">
+                    <Header />
+                    <div className="content">
+                        <Route exact path='/' render={() => <Home items={this.props.items}/>}/>
+                        <Route path='/cart' component={Cart}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = state =>{ // ф-ия, которая добавляет в props св-во items
+    return{
+        items: state.pizzas.items,
+    }
+}
+
+const mapDispatchToProps = dispatch => { // ф-ия, которая добавляет в props св-во - ф-ию setPizzas
+  return{
+      setPizzas: (items) => dispatch(setPizzasAction(items)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App); // связываем react c redux
--- src/redux/reducers/pizzas.js
+++ src/redux/reducers/pizzas.js
@@ -4,7 +4,6 @@ const initialState = {           // обязательные данные при
 
 const pizzas = (state = initialState, action) =>{    // reducer, который принимает state и action и возвращает измененный state
     if(action.type === 'SET_PIZZAS'){
-        debugger;
         return {
             ...state,
             items: action.payload,
